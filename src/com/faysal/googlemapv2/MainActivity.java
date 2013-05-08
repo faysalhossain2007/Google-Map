06c9b51a1cd1d4e12f3bef0d740dcdfb27c605bf
faysalhossain2007 <faysalhossain2007@gmail.com>
2013-05-09 04:05:50 +0600

    initial commit

--- src/com/faysal/googlemapv2/MainActivity.java
+++ src/com/faysal/googlemapv2/MainActivity.java
@@ -13,8 +13,8 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends Activity {
-	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-	static final LatLng KIEL = new LatLng(53.551, 9.993);
+	static final LatLng DESTINATION = new LatLng(73.58, 9.8);
+	static final LatLng FAYSAL = new LatLng(153.551, 9.993);
 	private GoogleMap map;
 
 	@Override
@@ -23,20 +23,18 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
 				.getMap();
-		Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-				.title("Hamburg"));
+		Marker myMarker = map.addMarker(new MarkerOptions().position(
+				DESTINATION).title("Hamburg"));
 		Marker kiel = map.addMarker(new MarkerOptions()
-				.position(KIEL)
-				.title("Kiel")
-				.snippet("Kiel is cool")
+				.position(FAYSAL)
+				.title("Faysal")
+				.snippet("HERE I AM")
 				.icon(BitmapDescriptorFactory
 						.fromResource(R.drawable.ic_launcher)));
 
-		// Move the camera instantly to hamburg with a zoom of 15.
-		map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(DESTINATION, 15));
 
-		// Zoom in, animating the camera.
-		map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+		map.animateCamera(CameraUpdateFactory.zoomTo(20), 1000, null);
 	}
 
 	@Override
